b8ff36c85ffaf07a11b8fae8b5732792435c25df
Soumyajit Banerjee <soumyajit4banerjee@gmail.com>
2022-02-22 12:17:37 +0530

    Project pushed

--- Accessmodifier/com/soumya/accessmodifier/proc1/Protectedclass.java
+++
@@ -1,9 +0,0 @@
-package com.soumya.accessmodifier.proc1;
-
-public class Protectedclass {
-     static protected int dataValue=300;
-     protected void protectedMethod()
-     {
-    	 System.out.println("I am a Protected method from class Protectedclass");
-     }
-}
--- Accessmodifier/com/soumya/accessmodifier/pub1/Publicclass.java
+++
@@ -1,9 +0,0 @@
-package com.soumya.accessmodifier.pub1;
-
-public class Publicclass {
-   static public int dataValue=90;
-   public void publicMethod()
-   {
-	   System.out.println("I am a public method from class Publicclass");
-   }
-}
